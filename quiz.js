b89d7d5c5e806891e62173cd48499821eda7c38d
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-06-14 17:07:39 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,22 +1,26 @@
 function checkAnswer() {
-    const correctAnswer = "4";
-    const selected = document.querySelector('input[name="quiz"]:checked');
-    const feedback = document.getElementById('feedback');
+  
+  const correctAnswer = "4";
 
- 
- if (!selected) {
+  const selected = document.querySelector('input[name="quiz"]:checked');
+  const feedback = document.getElementById('feedback');
+
+  if (!selected) {
     feedback.textContent = "Please select an answer.";
     return;
   }
+
   const userAnswer = selected.value;
 
-    if (userAnswer == correctAnswer){
-      feedback.textContent = "Correct! Well done.";
-    } else {
-     feedback.textContent = "That's incorrect. Try again!";
-    }
+
+  if (userAnswer === correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
 }
 
-document.getElementById("submit-answer").addEventListener('onclick', checkAnswer);
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
+
 
 
