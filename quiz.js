cea3a016e354218cc6728a1fa711ff8000ad1e38
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-06-14 17:06:21 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,22 +1,22 @@
 function checkAnswer() {
     const correctAnswer = "4";
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    const selected = document.querySelector('input[name="quiz"]:checked');
     const feedback = document.getElementById('feedback');
 
- document.getElementById("submit-answer").addEventListener('onclick', checkAnswer);
+ 
  if (!selected) {
     feedback.textContent = "Please select an answer.";
     return;
   }
-  userAnswer = selected.value;
+  const userAnswer = selected.value;
 
     if (userAnswer == correctAnswer){
       feedback.textContent = "Correct! Well done.";
     } else {
-      if(userAnswer != correctAnswer){
      feedback.textContent = "That's incorrect. Try again!";
-     }
     }
-  }
+}
+
+document.getElementById("submit-answer").addEventListener('onclick', checkAnswer);
 
 
