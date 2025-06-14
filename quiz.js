f200a83649e03a818167af66d7aa6f7ce4c5c280
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-06-14 07:51:01 +0200

    simple quiz

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+    document.querySelector(`"name="quiz"`);
+
+    const userAnswer = document.querySelector(`"name="quiz"`).value;
+
+    if (userAnswer == correctAnswer){
+      feedback.textContent = "Correct! Well done.";
+    } else {
+     feedback.textContent = "That's incorrect. Try again!";
+    }
+    document.getElementById("submit-answer").addEventListener('onclick', checkAnswer =>{
+
+    });
+}
