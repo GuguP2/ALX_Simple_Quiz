5dd9eb3c72cdc33579f1404aba17a21b67fd6b78
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-06-14 17:17:50 +0200

    simple calculator

---
+++ calculator.js
@@ -0,0 +1,6 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+document.addEventListener("onclick", (number1, number2)){
+  return document.getElementById("#calculation-result");
+}
